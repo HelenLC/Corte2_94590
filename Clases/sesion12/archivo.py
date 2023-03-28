c20609adff219f9c9806a9e92123c2da928e0b12
HelenLC <hjlopezc2@academia.usbbog.edu.co>
2023-03-28 15:09:02 -0500

    sesion12

---
+++ Clases/sesion12/archivo.py
@@ -0,0 +1,18 @@
+
+def main():
+    dicc={}
+    f=open('matrizAsignacion.txt','rt')
+    matriz = f.readlines()
+    f.seek(0)
+    suma=(0)
+    for i in range(len(matriz)):
+        valor= f.readline().rstrip('\n').split(',')
+        for j in range(len(valor)):
+            pos = str(f'{i}{j}')
+            dicc[pos] = valor[j]
+
+    print(dicc)
+    f.close()               
+
+if __name__=="__main__":
+    main()
---
+++ Clases/sesion12/diccionario.py
@@ -0,0 +1,7 @@
+datos={'nombre1':'Juan','nombre2':'pedro'}
+print(datos['nombre1'])
+print(datos['nombre2'])
+
+mancera={'edad':19,'documento':12763729,'genero':'M'}
+felipe={'edad':20,'documento':12763729,'genero':'M'}
+nombre={1:'edad',2:'documento'}
