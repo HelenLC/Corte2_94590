15cdcecc6b7147acd3cc12b355329dc0bb098661
HelenLC <hjlopezc2@academia.usbbog.edu.co>
2023-03-30 15:05:07 -0500

    sesion13

--- Clases/sesion13/IMC.py
+++ Clases/sesion13/IMC.py
@@ -5,8 +5,20 @@ class personas:
         self.altura=None
         self.peso=None
 
-def Indice(self):
-    IMC=self.peso/((self.altura/100)**2)
+    def Indice(self):
+        IMC=self.peso/((self.altura/100)**2)
+        if IMC<= 18.5:
+            return str('Por debajo')
+        elif IMC <=24.9:
+            return str('Saludable')
+        elif IMC <=29.9:
+            return str('Sobrepeso')
+        elif IMC <=34.9:
+            return str('Obesidad 1')
+        elif IMC <=39.9:
+            return str('Obesidad 2')
+        else:
+            return str('Obesidad 3')
 
 def main():
     estudiante1 = personas()
@@ -28,6 +40,11 @@ def main():
     estudiante4.nombre='Jessica Fuentes'
     estudiante4.altura=170
     estudiante4.peso=73
+
+    print(f'estudiante: {estudiante1.nombre} resultado: {estudiante1.Indice()}')
+    print(f'estudiante: {estudiante2.nombre} resultado: {estudiante2.Indice()}')
+    print(f'estudiante: {estudiante3.nombre} resultado: {estudiante3.Indice()}')
+    print(f'estudiante: {estudiante4.nombre} resultado: {estudiante4.Indice()}')
 
 if __name__:"__main__"
 main()
