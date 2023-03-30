825830e01eebd1b4b4f3f1566df220e6c77ec022
HelenLC <hjlopezc2@academia.usbbog.edu.co>
2023-03-30 14:43:19 -0500

    sesion13

---
+++ Clases/sesion13/IMC.py
@@ -0,0 +1,33 @@
+
+class personas:
+    def __init__(self):
+        self.nombre=None
+        self.altura=None
+        self.peso=None
+
+def Indice(self):
+    IMC=self.peso/((self.altura/100)**2)
+
+def main():
+    estudiante1 = personas()
+    estudiante1.nombre='Pedro Caceres'
+    estudiante1.altura=188
+    estudiante1.peso=97
+
+    estudiante2 = personas()
+    estudiante2.nombre='Maria Perez'
+    estudiante2.altura=160
+    estudiante2.peso=47
+
+    estudiante3 = personas()
+    estudiante3.nombre='Julian Dominguez'
+    estudiante3.altura=158
+    estudiante3.peso=58
+
+    estudiante4 = personas()
+    estudiante4.nombre='Jessica Fuentes'
+    estudiante4.altura=170
+    estudiante4.peso=73
+
+if __name__:"__main__"
+main()
